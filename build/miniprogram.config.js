c90a91f60c4f76710021d5954fb587c8f4cab6c0
deancn <dean@airdb.com>
2020-03-09 01:40:41 +0800

    first commit

---
+++ build/miniprogram.config.js
@@ -0,0 +1,81 @@
+/**
+ * 配置参考：https://wechat-miniprogram.github.io/kbone/docs/config/
+ */
+
+module.exports = {
+    origin: 'https://mina.baobeihuijia.com',
+    entry: '/',
+    router: {
+        home: [
+            '/(home|index)?',
+            '/index.html',
+            '/test/(home|index)',
+        ],
+        other: [
+            '/test/list/:id',
+            '/test/detail/:id',
+        ],
+        me: [
+            '/list/:id',
+            '/detail/:id',
+        ],
+    },
+    redirect: {
+        notFound: 'home',
+        accessDenied: 'home',
+    },
+    generate: {
+        autoBuildNpm: 'npm',
+  		tabBar: {
+			color: '#333',
+            selectedColor: '#EF4431',
+            backgroundColor: '#ffffff',
+            list: [{
+                pageName: 'home',
+                text: '主页',
+	            iconPath: './imgs/tabs/home.png',
+                selectedIconPath: './imgs/tabs/home_selected.png'
+            }, {
+                pageName: 'me',
+                text: '我的',
+                iconPath: './imgs/tabs/me.png',
+                selectedIconPath: './imgs/tabs/me_selected.png'
+            }],
+        },
+    },
+    app: {
+        backgroundTextStyle: 'light',
+        navigationBarBackgroundColor: '#fff',
+        navigationBarTitleText: 'Go 中文网',
+        navigationBarTextStyle: 'black'
+    },
+	appExtraConfig: {
+        sitemapLocation: 'sitemap.json',
+	},
+    global: {
+        share: true,
+        windowScroll: false,
+        backgroundColor: '#F7F7F7',
+    },
+    pages: {
+        home: {
+            enablePullDownRefresh: true,
+            pullDownRefresh: true,
+        },
+    },
+    optimization: {
+		domSubTreeLevel: 10,
+
+		elementMultiplexing: true,
+		textMultiplexing: true,
+		commentMultiplexing: true,
+		domExtendMultiplexing: true,
+
+		styleValueReduce: 5000,
+		attrValueReduce: 5000,
+	},
+    projectConfig: {
+        projectname: 'mina',
+        appid: 'wxf7b5556f34553852',
+    },
+}
---
+++ src/home/Index.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="cnt">
+    <KSwiper
+      :indicator-dots="indicatorDots"
+      :circular="circularFlag"
+      :autoplay="autoplay"
+      :duration="duration"
+      :interval="interval"
+    >
+    <KSwiperItem
+        v-for="(item,index) in background"
+        :key="index" >
+        <KView :class="'swiper-item '+ item">
+        <img :src="bannerImg" class="banner-item"/>
+        </KView>
+      </KSwiperItem>
+    </KSwiper>
+
+    <tabs></tabs>
+    <wx-button open-type="share" @click="onClickShare">分享</wx-button>
+
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Header from '../common/Header.vue'
+import Footer from '../common/Footer.vue'
+import Web from 'reduce-loader!../common/Web.vue'
+import 'reduce-loader!./web'
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+    Header,
+    Footer,
+    Web,
+  },
+
+  data() {
+    return {
+      interval: 2380,
+      autoplay: true,
+      indicatorDots: true,
+      circularFlag: true,
+      background: [
+        'https://wechat-1251018873.file.myqcloud.com/images/banner.png',
+        'https://wechat-1251018873.file.myqcloud.com/images/banner.png',
+        'https://wechat-1251018873.file.myqcloud.com/images/banner.png',
+      ],
+      bannerImg: 'https://wechat-1251018873.file.myqcloud.com/images/banner.png',
+    }
+  },
+
+  created() {
+    window.addEventListener('wxload', query => console.log('page1 wxload', query))
+    window.addEventListener('wxshow', () => console.log('page1 wxshow'))
+    window.addEventListener('wxready', () => console.log('page1 wxready'))
+    window.addEventListener('wxhide', () => console.log('page1 wxhide'))
+    window.addEventListener('wxunload', () => console.log('page1 wxunload'))
+
+    if (process.env.isMiniprogram) {
+      console.log('I am in miniprogram')
+      // For Dean Test Passport.
+      // Can delete if needed.
+      wx.login({
+        success(res) {
+          if (res.code) {
+            wx.request({
+              url: 'https://mina.baobeihuijia.com/apis/oauth/v1/wechat/login',
+              data: {
+                code: res.code
+              }
+            })
+          } else {
+            console.log('Login failed')
+          }
+        }
+      })
+    } else {
+      console.log('I am in Web')
+    }
+  },
+  methods: {
+    onClickJump() {
+      window.location.href = '/test/list/123'
+    },
+
+    onClickOpen() {
+      window.open('/test/detail/123')
+    },
+
+    onClickShare() {
+      console.log('I am in miniprogram')
+    },
+  },
+})
+</script>
+
+<style lang="less">
+.cnt {
+  margin-top: 20px;
+}
+
+a, button {
+  display: block;
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  font-size: 20px;
+  border: 1px solid #ddd;
+}
+
+.miniprogram-root {
+  .for-web {
+    display: none;
+  }
+}
+
+    .swiper {
+        margin-top: 15px;
+        width: 100%;
+        height: 185px;
+    }
+    .banner-item {
+        display: block;
+        margin: auto;
+        height: 160px;
+        border-radius: 20px;
+    }
+
+</style>
