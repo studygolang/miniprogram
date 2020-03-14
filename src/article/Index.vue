8ec4c27862a5cfed7370ae9ae195af0823090562
whx <2283439924@qq.com>
2020-03-14 16:57:45 +0800

    2020-03-14_whx

--- src/article/Index.vue
+++ src/article/Index.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="cnt">
-    <div>
-      首页文章内容页 id = {{this.$route.params.id}} <br>
-    </div>
+  <div class="page_article">
+    <header class="header">
+        <h1 class="title">
+            {{ article.title }} + {{ this.$route.params.id }}
+        </h1>
+        <p class="desc">
+            {{ article.description }}
+        </p>
+    </header>
+    <article class="content">
+        这里面是内容
+    </article>
   </div>
 </template>
 
@@ -11,8 +19,39 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'Article',
+  data() {
+    return {
+      article:{
+        author: {
+          avatar: 'http://img1.3lian.com/gif/more/11/2012/03/d037a77443c0a72a1432d815cd3b5724.jpg',
+          nickname: '昵称'
+        },
+        title: '什么是协程（goroutine），它们是怎样作的呢?',
+        description: '这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述这是描述',
+        like: 6,
+        comments: 10,
+        id: 12,
+        cover: ''
+      }
+    }
+  }
 })
 </script>
 <style lang="less">
-
+.page_article {
+     padding: 0 20px;
+    .header {
+        .title {
+            color:#333;
+            font-size: 34rpx;
+            border-bottom: 2px solid #eee;
+            padding: 10px 0;
+        }
+        .desc {
+            color:#555;
+            font-size: 30rpx;
+            margin: 20px 0 30px;
+        }
+    }
+}
 </style>
